e1765a42ad3a436f8f9602aafdf6c8b1b5e67706
Epichin <d.pena@hotmail.com>
2024-01-25 15:57:12 -0300

    Feat: adjusted so thath e-mail sends are unique to each address.

--- send_mail.py
+++ send_mail.py
@@ -12,26 +12,25 @@ sender_gmail = os.environ.get('SENDER_GMAIL')
 email_recipient = os.environ.get('EMAIL_RECIPIENT').split(', ')
 
 
-def send_mail(dolar_price, euro_price, btc_price):  
+def send_mail(dolar_price, euro_price, btc_price): 
     body_mail = f"""
     <h2>Cotação atualizada em {datetime.now()}</h2>
     <p>Cotação do Dolar: R${dolar_price:.2f}</p>
     <p>Cotação do Euro: R${euro_price:.2f}</p>
     <p>Cotação do Bitcoin: R${btc_price:.2f}</p>
     """
+    for email_path in email_recipient:
+        msg = email.message.Message()
+        msg['Subject'] = "Cotação diaria"
+        msg['From'] = f'{sender_gmail}'
+        msg['To'] = email_path
+        password = f'{pass_google}' 
+        msg.add_header('Content-Type', 'text/html')
+        msg.set_payload(body_mail)
 
-    msg = email.message.Message()
-    msg['Subject'] = "Cotação diaria"
-    msg['From'] = f'{sender_gmail}'
-    msg['To'] = ", ".join(email_recipient)
-    password = f'{pass_google}' 
-    msg.add_header('Content-Type', 'text/html')
-    msg.set_payload(body_mail)
-
-    s = smtplib.SMTP('smtp.gmail.com: 587')
-    s.starttls()
-    s.login(msg['From'], password)
-    # s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-    s.sendmail(msg['From'], email_recipient, msg.as_string().encode('utf-8'))
-    print('Email enviado')
-
+        s = smtplib.SMTP('smtp.gmail.com: 587')
+        s.starttls()
+        s.login(msg['From'], password)
+        s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+        print('Email enviado')
+    s.quit()
