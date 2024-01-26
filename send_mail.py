81ea1ef24ab2e3dd496b365c36d8d0d890f295be
Epichin <d.pena@hotmail.com>
2024-01-26 15:44:11 -0300

    Feat: change send mail one by one to one to all.

--- send_mail.py
+++ send_mail.py
@@ -19,18 +19,18 @@ def send_mail(dolar_price, euro_price, btc_price):
     <p>Cotação do Euro: R${euro_price:.2f}</p>
     <p>Cotação do Bitcoin: R${btc_price:.2f}</p>
     """
-    for email_path in email_recipient:
-        msg = email.message.Message()
-        msg['Subject'] = "Cotação diaria"
-        msg['From'] = f'{sender_gmail}'
-        msg['To'] = email_path
-        password = f'{pass_google}' 
-        msg.add_header('Content-Type', 'text/html')
-        msg.set_payload(body_mail)
 
-        s = smtplib.SMTP('smtp.gmail.com: 587')
-        s.starttls()
-        s.login(msg['From'], password)
-        s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-        print('Email enviado')
+    msg = email.message.Message()
+    msg['Subject'] = "Cotação diaria"
+    msg['From'] = f'{sender_gmail}'
+    msg['To'] = ', '.join(email_recipient)
+    password = f'{pass_google}' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(body_mail)
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], email_recipient, msg.as_string().encode('utf-8'))
+    print('Email enviado')
     s.quit()
